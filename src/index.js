496f5135fcd0f875d662c791d719f94f8ef0db8e
Efim Poleschuk <limitlessFIMA@gmail.com>
2018-02-22 10:43:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+   
+    let obj = {};
+
+    if(currency <= 0){
+        return {};
+    }
+    if(currency >= 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+
+    if(currency >= 50){
+        obj.H =parseInt(currency/50);
+        currency = currency - obj.H * 50;
+    }
+    if(currency >= 25){
+        obj.Q =parseInt(currency/25);
+        currency = currency - obj.Q * 25;
+    }
+    if(currency >= 10){
+        obj.D =parseInt(currency/10);
+        currency = currency - obj.D * 10;
+    }
+    if(currency >= 5){
+        obj.N =parseInt(currency/5);
+        currency = currency - obj.N * 5;
+    }
+    if(currency >= 1){
+        obj.P =parseInt(currency/1);
+        currency = currency - obj.P * 1;
+    }
+
+    return obj;
+
 }
